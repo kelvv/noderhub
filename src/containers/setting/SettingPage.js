666977774c8f6d87d41f35ec07fc01c94a02fd15
kelvv <kelvv@outlook.com>
2016-09-18 19:31:21 +0800

    add camera

--- src/containers/setting/SettingPage.js
+++ src/containers/setting/SettingPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {  StyleSheet , View ,Text} from 'react-native';
+import {  StyleSheet , View ,Text ,Dimensions} from 'react-native';
+import Camera from 'react-native-camera';
 
 class SettingPage extends Component {
   render() {
@@ -7,9 +8,25 @@ class SettingPage extends Component {
       <View style={styles.container}>
           <View style={styles.setting}>
               <View style={styles.line}/>
+              <View style={styles.container}>
+                <Camera
+                ref={(cam) => {
+                    this.camera = cam;
+                }}
+                style={styles.preview}
+                aspect={Camera.constants.Aspect.fill}>
+                <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+                </Camera>
+              </View>
           </View>
       </View>
     );
+  }
+
+  takePicture() {
+    this.camera.capture()
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
   }
 }
 
@@ -24,6 +41,21 @@ const styles = StyleSheet.create({
     },
     setting : {
 
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        height: Dimensions.get('window').height,
+        width: Dimensions.get('window').width
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        color: '#000',
+        padding: 10,
+        margin: 40
     }
 });
 
